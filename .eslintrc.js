0417ca11c62f118d267d1effe2979d68069e55d9
Andrey Kozlov <l-2732@yandex.ru>
2021-01-12 14:29:30 +0000

    lint tools for a start

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "root": true,
+    "env": {
+        "es2020": true,
+        "node": true
+    },
+    "extends": [
+        "airbnb"
+    ],
+    "parserOptions": {
+        "ecmaVersion": 11,
+        "sourceType": "module"
+    },
+    "plugins": [],
+    "rules": {
+      "import/prefer-default-export": 0
+    },
+    "overrides": []
+};
